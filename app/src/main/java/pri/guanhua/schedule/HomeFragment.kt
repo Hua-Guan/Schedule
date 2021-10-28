613be771ee15eb7d2348a98bcc84e5b344c96f42
HuaGuan <G18174078632@gmail.com>
2021-10-28 20:19:50 +0800

    更新

--- app/src/main/java/pri/guanhua/schedule/HomeFragment.kt
+++ app/src/main/java/pri/guanhua/schedule/HomeFragment.kt
@@ -1,13 +1,25 @@
 package pri.guanhua.schedule
 
 import android.os.Bundle
+import android.util.TypedValue
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import pri.guanhua.schedule.model.HomeRecyclerViewAdapter
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -16,8 +28,16 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
         recyclerView.layoutManager = lin
         val adapter = HomeRecyclerViewAdapter()
         recyclerView.adapter = adapter
-        val spaces = 88
+        val spaces = dp2px(12)
         val space = HomeRecyclerViewAdapter.SpacesItemDecoration(spaces)
         recyclerView.addItemDecoration(space)
     }
+
+    private fun dp2px(dp: Int): Int {
+        return TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP, dp.toFloat(),
+            resources.displayMetrics
+        ).toInt()
+    }
+
 }
--- app/src/main/java/pri/guanhua/schedule/MainActivity.kt
+++ app/src/main/java/pri/guanhua/schedule/MainActivity.kt
@@ -6,23 +6,44 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.add
 import androidx.fragment.app.commit
 import androidx.fragment.app.replace
+import android.util.TypedValue
+
+
+
 
 class MainActivity : AppCompatActivity() {
+    private val homeFragment = HomeFragment()
+    private val labelFragment = LabelFragment()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         if (savedInstanceState == null) {
             supportFragmentManager.commit {
                 setReorderingAllowed(true)
-                add<HomeFragment>(R.id.fragment_container_view)
+                add(R.id.fragment_container_view, homeFragment)
             }
         }
         val addLabel = findViewById<ImageButton>(R.id.add_label)
         addLabel.setOnClickListener {
             supportFragmentManager.commit {
                 setReorderingAllowed(true)
-                replace<LabelFragment>(R.id.fragment_container_view)
+                replace(R.id.fragment_container_view, labelFragment)
+            }
+        }
+        val addPlan = findViewById<ImageButton>(R.id.my_schedule)
+        addPlan.setOnClickListener {
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                replace(R.id.fragment_container_view, homeFragment)
             }
         }
     }
+
+    private fun dp2px(dp: Int): Int {
+        return TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP, dp.toFloat(),
+            resources.displayMetrics
+        ).toInt()
+    }
+
 }
