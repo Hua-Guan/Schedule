9cd1e39d54f0d1c2330529887c301e57e5532a7f
HuaGuan <G18174078632@gmail.com>
2021-10-30 14:22:44 +0800

    更新

--- app/src/main/java/pri/guanhua/schedule/MainActivity.kt
+++ app/src/main/java/pri/guanhua/schedule/MainActivity.kt
@@ -16,34 +16,38 @@ import android.widget.TextView
 class MainActivity : AppCompatActivity() {
     private val homeFragment = HomeFragment()
     private val labelFragment = LabelFragment()
+    private val addFragment = AddFragment()
     private lateinit var textViewOldColor : ColorStateList
     private lateinit var textViewLabel : TextView
     private lateinit var textViewSchedule : TextView
     private lateinit var textViewAdd : TextView
     private lateinit var addLabel : ImageButton
     private lateinit var mySchedule : ImageButton
+    private lateinit var addMemorandum : ImageButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         if (savedInstanceState == null) {
             supportFragmentManager.commit {
                 setReorderingAllowed(true)
-                add(R.id.fragment_container_view, homeFragment)
+                add(R.id.fragment_container_view, addFragment)
             }
         }
         initView()
         label()
         schedule()
+        add()
     }
 
     private fun initView(){
 
         addLabel = findViewById(R.id.add_label)
         mySchedule = findViewById(R.id.my_schedule)
+        addMemorandum = findViewById(R.id.add_plan)
         textViewLabel = findViewById(R.id.text_label)
         textViewSchedule = findViewById(R.id.text_schedule)
         textViewAdd = findViewById(R.id.text_add)
-        textViewOldColor = textViewAdd.textColors
+        textViewOldColor = textViewLabel.textColors
 
     }
 
@@ -54,10 +58,14 @@ class MainActivity : AppCompatActivity() {
                 setReorderingAllowed(true)
                 replace(R.id.fragment_container_view, labelFragment)
             }
-            mySchedule.setBackgroundResource(R.drawable.image_schedule)
-            textViewSchedule.setTextColor(textViewOldColor)
             addLabel.setBackgroundResource(R.drawable.image_label_selected)
             textViewLabel.setTextColor(Color.parseColor("#FF1493"))
+
+            addMemorandum.setBackgroundResource(R.drawable.image_add)
+            textViewAdd.setTextColor(textViewOldColor)
+
+            mySchedule.setBackgroundResource(R.drawable.image_schedule)
+            textViewSchedule.setTextColor(textViewOldColor)
         }
 
     }
@@ -70,9 +78,35 @@ class MainActivity : AppCompatActivity() {
                 replace(R.id.fragment_container_view, homeFragment)
             }
             mySchedule.setBackgroundResource(R.drawable.image_schedule_selected)
+            textViewSchedule.setTextColor(Color.parseColor("#FF1493"))
+
             addLabel.setBackgroundResource(R.drawable.image_label)
             textViewLabel.setTextColor(textViewOldColor)
-            textViewSchedule.setTextColor(Color.parseColor("#FF1493"))
+
+            addMemorandum.setBackgroundResource(R.drawable.image_add)
+            textViewAdd.setTextColor(textViewOldColor)
+
+        }
+
+    }
+
+    private fun add(){
+
+        addMemorandum.setOnClickListener {
+
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                replace(R.id.fragment_container_view, addFragment)
+            }
+            addMemorandum.setBackgroundResource(R.drawable.image_add_selected)
+            textViewAdd.setTextColor(Color.parseColor("#FF1493"))
+
+            mySchedule.setBackgroundResource(R.drawable.image_schedule)
+            textViewSchedule.setTextColor(textViewOldColor)
+
+            addLabel.setBackgroundResource(R.drawable.image_label)
+            textViewLabel.setTextColor(textViewOldColor)
+
         }
 
     }
